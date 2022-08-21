f12f3b17f6732e222aac4b40f1f4b4a6142b70d0
lasergangers <celloadrian29@protonmail.com>
2022-08-21 23:37:31 +0700

    feat: implement onboarding UI

---
+++ src/pages/Onboarding.tsx
@@ -0,0 +1,121 @@
+import { useEffect } from "react";
+import {
+  View,
+  Text,
+  Pressable,
+  StyleSheet,
+  Easing,
+  Animated,
+} from "react-native";
+
+const Onboarding = () => {
+  const animatedValue = new Animated.Value(0);
+  useEffect(() => {
+    Animated.loop(
+      Animated.sequence([
+        Animated.delay(2000),
+        Animated.timing(animatedValue, {
+          toValue: 2,
+          duration: 750,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+      ])
+    ).start();
+  }),
+    [animatedValue];
+
+  return (
+    <View style={styles.PageContainer}>
+      <View style={styles.HeroImageContainer}>
+        <Animated.Text
+          style={[
+            styles.HeroImage,
+            {
+              transform: [
+                {
+                  rotate: animatedValue.interpolate({
+                    inputRange: [0, 1, 2],
+                    outputRange: ["0deg", "30deg", "0deg"],
+                  }),
+                },
+              ],
+            },
+          ]}
+        >
+          👋
+        </Animated.Text>
+      </View>
+      <View style={styles.TextContainer}>
+        <Text style={styles.Title}>Hey! Welcome</Text>
+        <Text style={styles.Subtitle}>
+          You are clicks away from setting up your own personal weather station
+        </Text>
+      </View>
+      <View style={styles.ButtonGroup}>
+        <Pressable style={styles.Button}>
+          <Text style={styles.ButtonText}>Get Started</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+export default Onboarding;
+
+const gap = 20;
+
+const styles = StyleSheet.create({
+  PageContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f5f5f5",
+    paddingLeft: 25,
+    paddingRight: 25,
+  },
+  HeroImageContainer: {
+    marginTop: gap,
+    marginBottom: gap,
+  },
+  HeroImage: {
+    fontSize: 100,
+  },
+  TextContainer: {
+    alignItems: "center",
+    marginTop: gap,
+    marginBottom: gap,
+  },
+  Title: {
+    fontWeight: "700",
+    fontSize: 20,
+    color: "#05101a",
+    marginBottom: gap / 4,
+  },
+  Subtitle: {
+    fontWeight: "400",
+    fontSize: 14,
+    color: "#05101a",
+    opacity: 0.5,
+    textAlign: "center",
+    marginTop: gap / 4,
+  },
+  ButtonGroup: {
+    width: "100%",
+    marginTop: gap,
+    marginBottom: gap,
+  },
+  Button: {
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 15,
+    paddingBottom: 15,
+    backgroundColor: "#f2d264",
+    borderRadius: 7,
+  },
+  ButtonText: {
+    fontWeight: "600",
+    fontSize: 14,
+    color: "#05101a",
+  },
+});
